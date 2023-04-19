ffe5955c23850eba9c0af3d5b933b06e91cefda7
Jisu Han <jshcdi6658@gmail.com>
2023-04-19 15:33:32 +0900

    [Update] Video settings (sequentially play different video, reduced video size)

--- intro/video_script.js
+++ intro/video_script.js
@@ -1,3 +1,4 @@
+const videoPlayer = document.getElementById('video-player');
 const videoSources = [
     'images/intro_videos/1.mp4',
     'images/intro_videos/2.mp4',
@@ -6,23 +7,44 @@ const videoSources = [
     // Add more video sources here
 ];
 
-function getRandomVideoSource() {
-    const randomIndex = Math.floor(Math.random() * videoSources.length);
-    console.log(videoSources[randomIndex])
-    return videoSources[randomIndex];
+function getRandomSubarray(arr, size) {
+    var shuffled = arr.slice(0), i = arr.length, temp, index;
+    while (i--) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    console.log(shuffled)
+    return shuffled.slice(0, size);
 }
 
-function createVideoElement(src) {
-    const video = document.createElement('video');
-    video.src = src;
-    video.muted = true;
-    video.autoplay = true;
-    video.loop = true;
-    return video;
+function loadNextVideo(random_array, idx) {
+    // Remove the previous source element, if it exists
+    const oldSource = videoPlayer.querySelector('source');
+    if (oldSource) {
+        videoPlayer.removeChild(oldSource);
+    }
+
+    const source = document.createElement('source');
+    source.src = random_array[idx];
+    source.type = 'video/mp4';
+    videoPlayer.appendChild(source);
+    videoPlayer.load();
+    videoPlayer.play();
+    console.log(random_array[currentVideoIndex]);
 }
 
-const videoContainer = document.getElementById('video-container');
-const randomVideoSource = getRandomVideoSource();
-const randomVideoElement = createVideoElement(randomVideoSource);
+var random_array = getRandomSubarray(videoSources, 100000);
+let currentVideoIndex = 0;
+videoPlayer.addEventListener('ended', () => {
+    currentVideoIndex++;
+    if (currentVideoIndex >= random_array.length) {
+        currentVideoIndex = 0; // Loop back to the first video when the sequence ends
+    }
+    if (currentVideoIndex < videoSources.length) {
+        loadNextVideo(random_array, currentVideoIndex);
+    }
+});
 
-videoContainer.appendChild(randomVideoElement);
+loadNextVideo(random_array, currentVideoIndex);
